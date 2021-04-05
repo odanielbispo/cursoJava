f0b443c0832484f57eb5a6f7155a6f8ece702a5b
Daniel Bispo <danieubispo@gmail.com>
2021-04-05 16:45:04 -0300

    upload do exercício Bank Account

--- Product_/src/me/danielbispo/entities/Product.java
+++ Product_/src/me/danielbispo/entities/Product.java
@@ -5,33 +5,39 @@ public class Product {
     private double price;
     private int quantity;
 
-    public Product(){}
+    public Product() {
+    }
 
     public Product(String name, double price, int quantity) {
+        super();
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
 
-    public Product(String name, double price){
+    public Product(String name, double price) {
         this.name = name;
         this.price = price;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public double getPrice(){
-        return price;
-    }
-
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public void setPrice(double price){
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
         this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
     }
 
     public double totalValueInStock(){
