771aabc5782ef9e66e2645407e40ce265b6e3397
Daniel Bispo <danieubispo@gmail.com>
2021-04-06 14:54:45 -0300

    Exemplo de Foreach

---
+++ ForEach/src/me/danielbispo/application/ForEach.java
@@ -0,0 +1,11 @@
+package me.danielbispo.application;
+
+public class ForEach {
+    public static void main(String[] args) {
+        String[] vect = new String[]{"Maria", "Bob", "Alex"};
+
+        for (String obj : vect) {
+            System.out.println(obj);
+        }
+    }
+}
