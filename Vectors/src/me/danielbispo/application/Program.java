5321bc128006bdea148e4408d4c4e2df10fa35b4
Daniel Bispo <danieubispo@gmail.com>
2021-04-06 11:10:17 -0300

    Introdução a vetores

---
+++ Vectors/src/me/danielbispo/application/Program.java
@@ -0,0 +1,31 @@
+package me.danielbispo.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        //agora vou criar um vetor do tipo Double, que o tamanho dele é o valor atribuído
+        //na variável N.
+        double[] vect = new double[n];
+
+        for (int i = 0; i < n; i++){
+            vect[i] = sc.nextDouble();
+        }
+
+        double sum = 0.0;
+
+        for (int i = 0; i < n; i++){
+            sum += vect[i];
+        }
+        double avg = sum / n;
+
+        System.out.printf("AVERAGE HEIGHT: %.2f%n", avg);
+
+        sc.close();
+    }
+}
