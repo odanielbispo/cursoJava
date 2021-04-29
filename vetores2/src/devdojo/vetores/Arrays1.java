96f28264e6f0b56291bc3f87ad9f00466fb8fe3b
Daniel Bispo <danieubispo@gmail.com>
2021-04-29 13:52:30 -0300

    Revisão dos fundamentos sobre vetores

---
+++ vetores2/src/devdojo/vetores/Arrays1.java
@@ -0,0 +1,15 @@
+package devdojo.vetores;
+
+public class Arrays1 {
+    public static void main(String[] args) {
+        int [] idades = new int[3]; // int idades[], o primeiro é comumente usado (recomendado)
+        idades[0] = 20;
+        idades[1] = 15;
+        idades[2] = 30;
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+
+    }
+}
---
+++ vetores2/src/devdojo/vetores/Arrays2.java
@@ -0,0 +1,11 @@
+package devdojo.vetores;
+
+public class Arrays2 {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
