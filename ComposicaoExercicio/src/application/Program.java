508e09cc98392ef2c750d094928c7b6ff8ad628a
Daniel Bispo <danieubispo@gmail.com>
2021-06-16 12:14:39 -0300

    Sistema pedidos composicao: Product + OrderItem

---
+++ ComposicaoExercicio/src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product p = new Product("TV", 1000.00);
+
+        sc.close();
+    }
+}
