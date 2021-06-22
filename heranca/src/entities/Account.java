908180f7224b257004b7e187daf2bc134dd38030
Daniel Bispo <danieubispo@gmail.com>
2021-06-22 12:26:50 -0300

    Sobreposição e super classe em Herança

--- heranca/src/entities/Account.java
+++ heranca/src/entities/Account.java
@@ -35,8 +35,9 @@ public class Account {
         return balance;
     }
 
+
     public void withdraw(double amount){
-        balance -= amount;
+        balance -= amount + 5;
     }
 
     public void deposit(double amount){
